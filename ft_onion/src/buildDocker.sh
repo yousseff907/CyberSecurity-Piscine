aac52393e4fa227b91520d6f9c9ed4663fc4b7f6
youssef907 <youssef.itani.11.11@gmail.com>
2025-11-07 23:36:35 +0200

    v1 onion

--- ft_onion/src/buildDocker.sh
+++ ft_onion/src/buildDocker.sh
@@ -2,7 +2,7 @@
 echo "Building docker image"
 docker build -t ft_onion .
 echo "Running container"
-docker run -d --name ft_onion_container -p 4242:4242 ft_onion
+docker run -d --name ft_onion_container -p 2424:4242 ft_onion
 echo "Wait 30 seconds for initialization"
 sleep 30
 echo "Creating your .onion adress"
@@ -12,5 +12,5 @@ docker ps
 echo "Running services"
 docker exec ft_onion_container ps aux | grep 'ssh|tor|nginx'
 echo "For testing"
-echo "SSH: ssh -i ~/.ssh/ft_onion_key -p 4242 root@localhost"
+echo "SSH: ssh -i ~/.ssh/ft_onion -p 2424 root@localhost"
 echo "Web: Open Tor Browser and visit the .onion address above"
